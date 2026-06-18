38695ce404c37f3c410128bf2c57975079efdea5
Olawale Aregbe <96812426+OlaMarcus09@users.noreply.github.com>
2026-06-18 03:11:08 +0100

    Add comment to clarify component return
    Added a comment to clarify the return statement.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  // Directly returns the clean component cascade
   return <Component {...pageProps} />;
 }
